fa48d7cddc82652f9cc6c1a4ae4100cc741a0db2
hyder <yanghuan0613@gmail.com>
2019-06-02 19:47:43 +0800

    redux-thunk 获取异步数据

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { CSSTransition } from "react-transition-group";
-import { connect } from "react-redux";
+import {CSSTransition} from "react-transition-group";
+import {connect} from "react-redux";
 
-import { creator } from './store'
+import {creator} from './store'
 import {
   HeaderWrapper,
   Logo,
@@ -19,55 +19,85 @@ import {
   Button
 } from './style';
 
+class Header extends React.Component {
+  getListArea = (show) => {
+    if (this.props.focused) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>
+              换一换
+            </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            {
+              this.props.list.map((item, index) => {
+                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+              })
+            }
+          </SearchInfoList>
+        </SearchInfo>
+      );
+    } else {
+      return null;
+    }
+  };
 
-const Header = (props) => {
-  return (
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className={"left active"}>首页</NavItem>
-        <NavItem className={"left"}>App</NavItem>
-        <NavItem className={"right"}>登陆</NavItem>
-        <NavItem className={"right"}>
-          <i className={"iconfont"}>&#xe636;</i>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition
-            timeout={200}
-            in={props.focused}
-            className={"slide"}
-          >
-            <NavSearch
-              className={props.focused ? "focused" : ""}
-              onFocus={props.handleSearchFocus}
-              onBlur={props.handleSearchBlur}
-            />
-          </CSSTransition>
-          <i
-            className={props.focused ? "focused iconfont zoom" : "iconfont zoom"}
-          >&#xe614;</i>
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className={"writting"}>
-          <i className={"iconfont"}>&#xe615;</i>
-          写文章
-        </Button>
-        <Button className={"reg"}>注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  );
-};
+
+
+  render() {
+    return (
+      <HeaderWrapper>
+        <Logo/>
+        <Nav>
+          <NavItem className={"left active"}>首页</NavItem>
+          <NavItem className={"left"}>App</NavItem>
+          <NavItem className={"right"}>登陆</NavItem>
+          <NavItem className={"right"}>
+            <i className={"iconfont"}>&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              timeout={200}
+              in={this.props.focused}
+              className={"slide"}
+            >
+              <NavSearch
+                className={this.props.focused ? "focused" : ""}
+                onFocus={this.props.handleSearchFocus}
+                onBlur={this.props.handleSearchBlur}
+              />
+            </CSSTransition>
+            <i
+              className={this.props.focused ? "focused iconfont zoom" : "iconfont zoom"}
+            >&#xe614;</i>
+            {this.getListArea()}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className={"writting"}>
+            <i className={"iconfont"}>&#xe615;</i>
+            写文章
+          </Button>
+          <Button className={"reg"}>注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.getIn(['header', "focused"])
+    focused: state.getIn(['header', "focused"]),
+    list: state.getIn(['header', 'list'])
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleSearchFocus() {
+      dispatch(creator.getTagList());
       dispatch(creator.searchFocus());
     },
     handleSearchBlur() {
--- src/common/header/store/actionCreator.js
+++ src/common/header/store/actionCreator.js
@@ -1,4 +1,6 @@
 import * as constants from './contants'
+import { fromJS } from "immutable";
+import Axios from "axios";
 
 export const searchFocus = () => {
   return {
@@ -9,5 +11,23 @@ export const searchFocus = () => {
 export const searchBlur = () => {
   return {
     type: constants.SEARCH_BLUR
+  }
+};
+
+export const changeList = (data) => {
+    return {
+      type: constants.CHANGE_LIST,
+      data: fromJS(data)
+    }
+};
+
+export const getTagList = () => {
+  return (dispatch) => {
+    Axios.get('/api/headerList.json').then((res) => {
+      const data = res.data;
+      dispatch(changeList(data.data));
+    }).catch((e) => {
+        console.log(e)
+    })
   }
 };
