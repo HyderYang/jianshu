189788cf028ea67f762a98d1149ecf61fe309afd
hyder <yanghuan0613@gmail.com>
2019-06-02 13:55:22 +0800

    搜索框动画

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { CSSTransition } from "react-transition-group";
 import {
   HeaderWrapper,
   Logo,
@@ -20,9 +20,21 @@ export default class Header extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-
+      focused: false
     }
   }
+
+  handleSearchFocus = () => {
+      this.setState({
+        focused: true
+      })
+  };
+
+  handleSearchBlur = () => {
+    this.setState({
+      focused: false
+    })
+  };
   
   render() {
     return (
@@ -36,8 +48,20 @@ export default class Header extends React.Component{
             <i className={"iconfont"}>&#xe636;</i>
           </NavItem>
           <SearchWrapper>
-            <NavSearch></NavSearch>
-            <i className={"iconfont zoom"}>&#xe614;</i>
+            <CSSTransition
+              timeout={200}
+              in={this.state.focused}
+              className={"slide"}
+            >
+              <NavSearch
+                className={this.state.focused ? "focused" : ""}
+                onFocus={this.handleSearchFocus}
+                onBlur={this.handleSearchBlur}
+              />
+            </CSSTransition>
+            <i
+              className={this.state.focused ? "focused iconfont zoom" : "iconfont zoom"}
+            >&#xe614;</i>
           </SearchWrapper>
         </Nav>
         <Addition>
