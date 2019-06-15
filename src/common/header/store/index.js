89ca5c38d89397b857198aa9decd7ded24efdd5f
hyder <yanghuan0613@gmail.com>
2019-06-15 21:20:31 +0800

    none

--- src/common/header/store/index.js
+++ src/common/header/store/index.js
@@ -2,4 +2,4 @@ import reducer from './reducer'
 import * as constants from './contants'
 import * as creator from './actionCreator'
 
-export { reducer, constants, creator };
+export {reducer, constants, creator};
